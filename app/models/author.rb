82d3fec1d6d935f9e9ba73c23868e4da9407643e
Darren DCosta <ddcosta@gmail.com>
2019-06-04 18:49:38 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,11 @@
-# frozen_string_literal: true
-
 class Author < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :phone_number, length: { is: 10 }
+
+  FactoryBot.define do
+    factory :author do
+      name "Darren"
+      phone_number "3015184792"
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -16,7 +16,15 @@ class Post < ActiveRecord::Base
 
   def clickbait?
     return unless CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
-
     errors.add(:title, "must be clickbait")
+  end
+
+  FactoryBot.define do
+    factory :post do
+      title "Secret"
+      content "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat."
+      summary "Awesome"
+      category "Fiction"
+    end
   end
 end
